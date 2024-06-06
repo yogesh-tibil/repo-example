503e50f0de5f98c4545707d4aba5920f2de84cdc
yogesh-tibil <yogesh.manjappa@tibilsolutions.com>
2024-06-06 13:53:38 +0530

    my first commit

---
+++ Basic.py
@@ -0,0 +1,17 @@
+#Python program to find the minimum among five numbers.
+'''a=int(input("enter a number"))
+b=int(input("enter b number"))
+c=int(input("enter c number"))
+d=int(input("enter d number"))
+e=int(input("enter e number"))
+if a<b and a<c and a<d and a<e:
+    print(a)
+elif b<c and b<d and b<e:
+    print(b)
+elif c<d and c<e:
+    print(c)
+elif d<e:
+    print(d)
+else:
+    print(e)'''
+
